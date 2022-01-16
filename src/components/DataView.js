40403a45cb35b8b169dd8ed4ee629efdfc5fa56d
sumi005 <salli7635@richmondhillhs.org>
2022-01-16 18:05:28 +0000

    street-easy-changes

--- src/components/DataView.js
+++ src/components/DataView.js
@@ -11,13 +11,18 @@ function DataView(props) {
       </div>
       <div className="view-column">
         <div className="data-view">
-          <div className="data-view-column left right">
+          <div className="data-view-column-left-right">
             <div className="text">
               <h3 className="text-header">Renters</h3>
             </div>
             <div className="text">
               <h2 className="text-data">{props.renterCost}</h2>
               <h3 className="text-label">Annual Housing Cost</h3>
+              <div className="text bottom">
+                  <h2 className="text-data">{props.renterIncome}</h2>
+                  <h3 className="text-label">Annual Income</h3>
+                </div>
+
             </div>
           </div>
           <div className="data-view-column right">
@@ -25,8 +30,26 @@ function DataView(props) {
               <h3 className="text-header">Homeowners</h3>
             </div>
             <div className="text">
-              <h2 className="text-data">{}</h2>
+              <h2 className="text-data">{props.ownerCost}</h2>
               <h3 className="text-label">Annual Housing Cost</h3>
+              <div className="text bottom">
+  <h2 className="text-data">{props.ownerIncome}</h2>
+  <h3 className="text-label">Annual Income</h3>
+</div>
+
+              
+                 <div className="text">
+                      <h3 className="text-header">Cost-to-Income ratio</h3>
+                 </div>
+                      <div className="text">
+                      <h2 className="text-data">{props.costRatio}</h2>
+                           <h3 className="text-label">Annual Housing Cost</h3>
+                           </div>
+                  <div className="text bottom">
+                     <h2 className="text-data">{props.incomeRatio}</h2>
+                        <h3 className="text-label">Annual Income</h3>
+                  </div>
+              
             </div>
           </div>
         </div>
